0583c516fdc771abe542feac105065da8c4726c1
Bryan Dedeurwaerder <23252092+BryanDedeur@users.noreply.github.com>
2021-01-19 01:08:07 -0800

    Added edge class to graph

---
+++ Assets/Scenes/Graph/Edge.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[System.Serializable]
+public class Edge : MonoBehaviour {
+
+    public Vertex vertex1 = null;
+    public Vertex vertex2 = null;
+    public float cost = 0;
+
+    public void Awake()
+    {
+        transform.parent = GameObject.Find("Graph").transform;
+    }
+    public void RefreshConnection()
+    {
+        Vector3 offset = vertex2.transform.position - vertex1.transform.position;
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, offset.magnitude);
+        transform.position = vertex1.transform.position + (offset / 2f);
+        transform.LookAt(vertex2.transform);
+    }
+
+}
